1218cdf8cedf12d578e37dc925708896fd939fac
carlosDeucher <carloshenriquedeucher@gmail.com>
2025-04-06 21:06:40 -0300

    feat: components

---
+++ components/Box.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+    sx?: string;
+}>()
+</script>
+
+<template>
+    <div class="d-block" :style="props.sx">
+        <slot></slot>
+    </div>
+</template>
---
+++ components/base/Link.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useTheme } from 'vuetify/lib/composables/theme.mjs';
+
+const props = defineProps<{
+    href: string;
+}>()
+
+const theme = useTheme();
+
+const secondaryColor = theme.current.value.colors.secondary;
+
+</script>
+
+<style scoped>
+a:hover {
+    text-decoration: underline !important;
+}
+</style>
+
+
+<template>
+    <a class="text-decoration-none font-weight-bold" :href="props.href" :style="{ color: secondaryColor }">
+        <slot></slot>
+    </a>
+</template>
