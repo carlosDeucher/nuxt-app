52c5745a424622c1bb3b0c8cea68735ff7a52835
carlosDeucher <carloshenriquedeucher@gmail.com>
2025-04-12 08:51:00 -0300

    feat: success step

--- pages/signup/pages/SuccessStep.vue
+++ pages/signup/pages/SuccessStep.vue
@@ -1,3 +1,30 @@
-<template>
+<script setup lang="ts">
+import { useDisplay } from 'vuetify'
 
+const { mobile } = useDisplay()
+
+</script>
+
+<template>
+    <v-container class="pa-0">
+        <v-card :elevation="mobile ? 0 : 1" class="pa-md-8 pa-4">
+            <h1 class="text-h5 text-center font-weight-bold ">
+                Cadastro concluído com sucesso!
+            </h1>
+            <h2 class="text-h6 text-center mt-4">A partir de agora você já pode explorar o sistema
+                Imobia!</h2>
+
+            <p class="text-body-1 text-center mt-6">
+                Alguns recursos permanecem limitados até
+                que você crie uma conta na plataforma de pagamentos ASAAS. Esse cadastro é simples e integrado ao
+                sistema.
+            </p>
+
+            <Stack class="flex-column ga-3 align-center mt-6">
+                <v-btn size="large" class="text-none" variant="flat" color="primary" href="">Criar conta ASAAS</v-btn>
+                <BaseLink class="text-center" href="/">Explorar sistema agora mesmo</BaseLink>
+            </Stack>
+
+        </v-card>
+    </v-container>
 </template>
