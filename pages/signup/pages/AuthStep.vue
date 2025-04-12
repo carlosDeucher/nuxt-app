5b949e9b193be72af88b48ff3a1ae5533690e8e4
carlosDeucher <carloshenriquedeucher@gmail.com>
2025-04-12 08:50:02 -0300

    feat: form improvents

--- pages/signup/pages/AuthStep.vue
+++ pages/signup/pages/AuthStep.vue
@@ -7,9 +7,10 @@
         <Stack class="flex-column ga-3">
             <v-text-field hide-details="auto" variant="outlined" label="E-mail" v-model="form.email" required
                 type="email" />
-            <v-text-field hide-details="auto" variant="outlined" label="Senha" v-model="form.password" required />
-            <v-text-field hide-details="auto" variant="outlined" label="Confirme sua senha"
-                v-model="form.confirmPassword" required />
+            <v-text-field type="password" :error-messages="formErrors.password || undefined" hide-details="auto"
+                variant="outlined" label="Senha" v-model="form.password" required />
+            <v-text-field type="password" :error-messages="formErrors.confirmPassword || undefined" hide-details="auto"
+                variant="outlined" label="Confirme sua senha" v-model="form.confirmPassword" required />
             <v-btn type="submit" size="large" color="primary" variant="flat" class="text-none">Enviar
             </v-btn>
             <Stack class="justify-center">
@@ -22,15 +23,35 @@
 </template>
 
 <script setup lang="ts">
+import type { saveAuthDataParams } from '../index.vue';
 const form = reactive({
     email: '',
     password: '',
     confirmPassword: '',
 })
 
+const formErrors = reactive({
+    email: '',
+    password: '',
+    confirmPassword: '',
+})
+
+const emit = defineEmits<{
+    "save-auth-data": [saveAuthDataParams]
+}>();
+
 const handleSubmit = () => {
-    console.log('Form submitted:', form)
-    // You can send this to an API using useFetch or $fetch
+    if (form.password.length < 8) {
+        formErrors.password = 'A senha deve conter no mínimo 8 caracteres';
+        return;
+    }
+
+    if (form.password !== form.confirmPassword) {
+        formErrors.confirmPassword = 'As senhas não coincidem';
+        return;
+    }
+
+    emit("save-auth-data", { email: form.email, password: form.password })
 }
 
 </script>
